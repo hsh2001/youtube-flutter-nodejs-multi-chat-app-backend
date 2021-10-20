5438ef44b86fdddf4c58069f1d77db8f5c27d70b
Asher Hwang <dev@hsh.kr>
2021-10-20 23:51:25 +0900

    Create express app

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log(2);
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.end("Hello " + request.query.name);
+});
+
+app.get("/search/:keyword", (request, response) => {
+  const { keyword } = request.params;
+  response.end(keyword);
+});
+
+app.listen(3000, () => {
+  console.log("서버가 정상적으로 실행되었어요!");
+});
